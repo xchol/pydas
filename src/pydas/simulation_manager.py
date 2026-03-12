a3e0c89d2d15636a433dee97d71ac79143c72f52
Christoffer Olofsson <christoffer.olofsson@hb.se>
2026-03-12 16:22:51 +0100

    Refactored simulation code, added additional test for upcoming 2R2C model

--- src/pydas/simulation_manager.py
+++ src/pydas/simulation_manager.py
@@ -3,24 +3,35 @@ import pandas as pd
 
 
 class SimpleBuilding:
-    def __init__(self, thermal_conductance, thermal_capacitance):
+    def __init__(self, thermal_conductance, thermal_capacitance, RC_type):
         self.thermal_conductance = thermal_conductance
         self.thermal_capacitance = thermal_capacitance
+        self.RC_type = RC_type # The type of RC-network model to be used in the thermal simulation.
         self.indoor_temperature = 20.0 # Degrees Celsius.
         self.temperature_setpoint_day = 21.0 # Degrees Celsius
         self.temperature_setpoint_night = 18.0 # Degrees Celsius
+    
+    def _1R1C_callback(self, timestep: float, external_heating: float, current_outdoor_temperature: float) -> None:
+        current_indoor_temperature = self.indoor_temperature
+        self.indoor_temperature += (timestep / self.thermal_capacitance) * \
+        (external_heating - self.thermal_conductance * (current_indoor_temperature - current_outdoor_temperature))
 
     def setpoint_temperature_callback(self, time_of_day) -> float:
         is_day = (8 <= time_of_day <= 22)
         return self.temperature_setpoint_day if is_day else self.temperature_setpoint_night
+
+    def update_building_temperature(self, timestep: float, external_heating: float, current_outdoor_temperature: float) -> None:
+        if self.RC_type == "1R1C": self._1R1C_callback(timestep, external_heating, current_outdoor_temperature)
+        elif self.RC_type == "2R2C": pass
+        else: assert False, "Please specify a RC-network type."
 
 
 class SupplyController:
     def __init__(self, maximum_heating_power):
         self.maximum_heating_power = maximum_heating_power
 
-    def setpoint_schedule(self, indoor_temperature: float, setpoint_temperature: float) -> float:
-        is_below_setpoint = (indoor_temperature <= setpoint_temperature)
+    def setpoint_schedule(self, building: SimpleBuilding, time_of_day: float) -> float:
+        is_below_setpoint = (building.indoor_temperature <= building.setpoint_temperature_callback(time_of_day = time_of_day))
         return self.maximum_heating_power if is_below_setpoint else 0.0
 
 
@@ -50,11 +61,8 @@ class BuildingEnergySimulator:
         self.outdoor_temperature_data = dataframe
 
     def step(self) -> None:
-        current_indoor_temperature = self.building.indoor_temperature
         current_outdoor_temperature = self.outdoor_temperature_data["Lufttemperatur"][self.current_time_step]
         time_of_day = self.outdoor_temperature_data["timestamp"].dt.hour[self.current_time_step]
-        current_temperature_setpoint = self.building.setpoint_temperature_callback(time_of_day)
-        current_heating_power = self.controller.setpoint_schedule(indoor_temperature = current_indoor_temperature, setpoint_temperature = current_temperature_setpoint)
-        self.building.indoor_temperature += (self.timestep / self.building.thermal_capacitance) * \
-              (current_heating_power - self.building.thermal_conductance * (current_indoor_temperature - current_outdoor_temperature))
+        current_heating_power = self.controller.setpoint_schedule(building = self.building, time_of_day = time_of_day)
+        self.building.update_building_temperature(self.timestep, current_heating_power, current_outdoor_temperature)
         self.current_time_step += 1
