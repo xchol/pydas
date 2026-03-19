0badb547e60b9636b7d806203b590f9beaf59338
Christoffer Olofsson <christoffer.olofsson@hb.se>
2026-03-19 14:02:30 +0100

    Added 4R3C model with ground temperature, added to RCBuiling class, made a notebook that experiments with this.

--- src/pydas/RCBuildings.py
+++ src/pydas/RCBuildings.py
@@ -20,6 +20,9 @@ class RCBuilding:
     def step(self, current_external_heating: float, current_outdoor_temperature: float) -> None:
         raise NotImplementedError("You are required to implement the time stepping update for an RCBuilding.")
     
+    def set_state(self, new_state: dict) -> None:
+        self.state = new_state
+
     def set_day_night_setpoints(self, setpoint_day: float, setpoint_night: float, daytime_from: int, daytime_to: int) -> None:
         self.setpoint_day = setpoint_day
         self.setpoint_night = setpoint_night
@@ -63,29 +66,34 @@ class RCBuilding2R2C(RCBuilding):
         self.current_timestep += 1
 
 
-class RCBuilding3R2C(RCBuilding):
-    def __init__(self, parameters):
-        super().__init__(parameters)
-        self.wall_temperature = 20.0 # Degree Celsius.
-        self.state = {"indoor_temperature": self.indoor_temperature, "wall_temperature": self.wall_temperature}
-        G_a, G_w = self.parameters["thermal_conductance_air"], self.parameters["thermal_conductance_wall"]
-        k_a, k_w = self.parameters["thermal_capacitance_air"], self.parameters["thermal_capacitance_wall"]
-        mass_flow = self.parameters["mass_flow"]
-        self.system_matrix = np.array([
-            [-(G_a + mass_flow * CP_WATER) / k_a, G_a / k_a],
-            [G_a / k_w, -(G_a + G_w) / k_w]
-        ])
+class RCBuilding4R3C(RCBuilding):
+    def __init__(self, thermal_resistances, thermal_capacitances, timestep):
+        super().__init__(thermal_resistances, thermal_capacitances, timestep)
+        self.state = {"indoor_temperature": self.indoor_temperature, "T2": 20.0, "T3": 20.0} # Initial conditions for the two states of the model.
+        self.input = {"outdoor_temperature": None, "ground_temperature": None, "external_heating": None}
+        self.C1_inv = 1.0 / self.thermal_capacitances["C1"]
+        self.R1C1_inv = 1.0 / (self.thermal_resistances["R1"] * self.thermal_capacitances["C1"])
+        self.R2C1_inv = 1.0 / (self.thermal_resistances["R2"] * self.thermal_capacitances["C1"])
+        self.R2C2_inv = 1.0 / (self.thermal_resistances["R2"] * self.thermal_capacitances["C2"])
+        self.R3C2_inv = 1.0 / (self.thermal_resistances["R3"] * self.thermal_capacitances["C2"])
+        self.R3C3_inv = 1.0 / (self.thermal_resistances["R3"] * self.thermal_capacitances["C3"])
+        self.R4C3_inv = 1.0 / (self.thermal_resistances["R4"] * self.thermal_capacitances["C3"])
+        
+        self.system_matrix = np.array([[-(self.R1C1_inv + self.R2C1_inv), self.R2C1_inv, 0.0], 
+                                       [self.R2C2_inv, -(self.R2C2_inv + self.R3C2_inv), self.R3C2_inv], 
+                                       [0.0, self.R3C3_inv, -(self.R3C3_inv + self.R4C3_inv)]])         
         self.system_matrix_inv = np.linalg.inv(self.system_matrix)
-        self.input_matrix = np.array([
-            [0.0, mass_flow * CP_WATER / k_a],
-            [G_w / k_w, 0.0]
-        ])
+        self.input_matrix = np.array([[0.0, self.R1C1_inv, self.C1_inv], 
+                                      [0.0, 0.0, 0.0], 
+                                      [self.R4C3_inv, 0.0, 0.0]])
     
-    def update_rcbuilding_temperature(self, timestep: float, current_supply_temperature: float, current_outdoor_temperature: float) -> None:
-        current_state = np.array([self.state["indoor_temperature"], self.state["wall_temperature"]])
-        current_input = np.array([current_outdoor_temperature, current_supply_temperature])
-        system_step_matrix = expm(self.system_matrix * timestep)
-        input_step_matrix = self.system_matrix_inv @ (system_step_matrix - np.identity(2)) @ self.input_matrix
+    def step(self) -> None:
+        current_state = np.array([self.state["indoor_temperature"], self.state["T2"], self.state["T3"]])
+        current_input = np.array([self.input["outdoor_temperature"], self.input["ground_temperature"], self.input["external_heating"]])
+        system_step_matrix = expm(self.system_matrix * self.timestep)
+        input_step_matrix = self.system_matrix_inv @ (system_step_matrix - np.identity(3)) @ self.input_matrix
         new_state = system_step_matrix @ current_state + input_step_matrix @ current_input
-        self.state["indoor_temperature"] = new_state[0]
-        self.state["wall_temperature"] = new_state[1]
+        self.state["indoor_temperature"], self.state["T2"], self.state["T3"] = new_state
+        self.current_timestep += 1
+
+
